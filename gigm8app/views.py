cec52745fec8ff4df1abfd5803e304476bbf5002
Edgar Perez <edg99@hotmail.com>
2016-04-12 15:20:36 +0100

    Configuring Heroku
    Configuring Heroku

--- gigm8app/views.py
+++ gigm8app/views.py
@@ -1,10 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
 
-import requests
-import json
-
-
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render(request))
