82a73fff1c7dfb87698288b9dba3d89a25a2bdc6
Edgar Perez <edg99@hotmail.com>
2016-04-19 23:09:55 +0100

    Change incorrect message
    Change incorrect message

--- gigm8/static/js/EventsbyLocation.js
+++ gigm8/static/js/EventsbyLocation.js
@@ -123,7 +123,7 @@ function getArtistByName(page, isPaginatorInit) {
             container.append(
                 '<div class="row">' +
                     ' <div class="col-sm-12 text-center" >' +
-                        "<h4 class='media-heading'>We didn't find any artist with these name :( </h4>" +
+                        "<h4 class='media-heading'>We didn't find any artist with this name :( </h4>" +
                     '</div>' +
                 '</div>'
             );
@@ -190,7 +190,7 @@ function renderEvents(data, isPaginatorInit){
     container.append(
         '<div class="row">' +
             ' <div class="col-sm-12 text-center" >' +
-                '<h4 class="media-heading">Take a look to this gigs :) </h4>' +
+                '<h4 class="media-heading">Take a look to these gigs :) </h4>' +
             '</div>' +
         '</div>'
     );
